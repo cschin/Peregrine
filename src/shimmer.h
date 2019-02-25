8d8012e5b994afed91ec99bd2e50cd9e24673864
Jason <cschin@infoecho.net>
2019-02-24 19:21:29 -0800

    proper changes for mm_utils.c

--- src/shimmer.h
+++ src/shimmer.h
@@ -14,6 +14,9 @@ void mm_sketch(void *km, const char *str, int len, int w, int k, uint32_t rid, i
 void mm_select(mm128_v *, mm128_v *,  uint8_t);
 uint32_t ks_ksmall_uint32_t(size_t n, uint32_t arr[], size_t kk);
 
+void write_mmlist(char *, mm128_v *); 
+mm128_v read_mmlist(char *); 
+
 #ifdef __cplusplus
 }
 #endif
