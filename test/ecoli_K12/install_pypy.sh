be4435d5836deab54e8cb31a0b1c7ecfc9bb5575
Jason Chin <cschin@infoecho.net>
2019-03-08 07:44:01 -0800

    add a simple testing case for E. coli assembly, we need to figure how to the genome sequences of K12MG1655.fa automatically later

---
+++ test/ecoli_K12/install_pypy.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y pypy
+wget -q https://bootstrap.pypa.io/get-pip.py
+sudo pypy get-pip.py
+sudo pypy -m pip install networkx
---
+++ test/ecoli_K12/run_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+find ./reads/ -name "reads_*.fa" > seq_dataset.lst
+SHIMMER=../../..
+SHIMMER=$(cd "$(dirname "../../../")"; pwd)/$(basename "$1")
+SHIMMERBIN=$SHIMMER/src
+echo get SHIMMER binaries from $SHIMMER
+mkdir -p ./wd/index
+mkdir -p ./wd/ovlp
+INDEX=./wd/index
+OVLOUT=./wd/ovlp
+echo
+echo build read index
+time (/usr/bin/time $SHIMMERBIN/build_read_index -p $INDEX/seq_dataset -d seq_dataset.lst 2> build_db.log)
+echo
+echo build shimmer index
+time (for c in `seq 1 6`; do echo "/usr/bin/time $SHIMMERBIN/build_shimmer_index -i $INDEX/seq_dataset.idx -t 6 -c $c -o $INDEX/shimmer 2> build_index.$c.log" ; done | parallel -j 4)
+echo
+echo build overlaps
+time (for c in `seq -f "%02g" 1 8`; do echo "/usr/bin/time $SHIMMERBIN/shimmer_to_overlap -p $INDEX/seq_dataset -l $INDEX/shimmer-L2 -t 8 -c $c > $OVLOUT/out.$c 2> ovlp.$c.log"; done | parallel -j 4)
+echo
+echo faclon ovlp to graph
+mkdir -p ./wd/asm
+cd ./wd/asm
+time (cat ../ovlp/out.* | $SHIMMERBIN/dedup_overlaps > preads.ovl; echo "-" >> preads.ovl)
+cp $SHIMMER/py/graph_to_contig.py .
+cp $SHIMMER/py/ovlp_to_graph.py .
+/usr/bin/time pypy ovlp_to_graph.py >& asm.log
+ln -sf ../index/seq_dataset.* .
+/usr/bin/time pypy graph_to_contig.py >& to_contig.log
