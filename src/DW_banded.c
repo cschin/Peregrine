4d6cc95c634b212e5c2bb1633a9b7b540445c76d
Jason Chin <cschin@infoecho.net>
2019-03-09 07:25:13 +0000

    we don't need to keep the alignment path, remove the related variables and memory allocation

--- src/DW_banded.c
+++ src/DW_banded.c
@@ -52,8 +52,6 @@
  # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  # SUCH DAMAGE.
  #################################################################################$$
-
-
 */
 
 #include <stdlib.h>
@@ -61,46 +59,6 @@
 #include <limits.h>
 #include <stdbool.h>
 #include "shimmer.h"
-
-int compare_d_path(const void * a, const void * b)
-{
-    const d_path_data2 * arg1 = a;
-    const d_path_data2 * arg2 = b;
-    if (arg1->d - arg2->d == 0) {
-        return  arg1->k - arg2->k;
-    } else {
-        return arg1->d - arg2->d;
-    }
-}
-
-
-void d_path_sort( d_path_data2 * base, unsigned long max_idx) {
-    qsort(base, max_idx, sizeof(d_path_data2), compare_d_path);
-}
-
-d_path_data2 * get_dpath_idx( seq_coor_t d, seq_coor_t k, 
-		unsigned long max_idx, d_path_data2 * base) {
-    d_path_data2 d_tmp;
-    d_path_data2 *rtn;
-    d_tmp.d = d;
-    d_tmp.k = k;
-    rtn = (d_path_data2 *)  bsearch( &d_tmp, base, max_idx, sizeof(d_path_data2), compare_d_path);
-    //printf("dp %ld %ld %ld %ld %ld %ld %ld\n", (rtn)->d, (rtn)->k, (rtn)->x1, (rtn)->y1, (rtn)->x2, (rtn)->y2, (rtn)->pre_k);
-
-    return rtn;
-
-}
-
-void print_d_path(  d_path_data2 * base, unsigned long max_idx) {
-    unsigned long idx;
-    for (idx = 0; idx < max_idx; idx++){
-        printf("dp %ld %d %d %d %d %d %d %d\n",idx, 
-				(base+idx)->d, (base+idx)->k, 
-				(base+idx)->x1, (base+idx)->y1, 
-				(base+idx)->x2, (base+idx)->y2, (base+idx)->pre_k);
-    }
-}
-
 
 alignment * align(char * query_seq, seq_coor_t q_len,
                   char * target_seq, seq_coor_t t_len,
@@ -113,22 +71,19 @@ alignment * align(char * query_seq, seq_coor_t q_len,
     seq_coor_t best_m;  // the best "matches" for each d
     seq_coor_t min_k, new_min_k;
     seq_coor_t max_k, new_max_k;
-    seq_coor_t pre_k;
     seq_coor_t x, y;
+    seq_coor_t x1, y1;
     seq_coor_t max_d;
     seq_coor_t band_size;
 	bool start = false;
-    unsigned long d_path_idx = 0;
 
-    d_path_data2 * d_path;
-    path_point * aln_path;
     alignment * align_rtn;
     bool aligned = false;
 
     //printf("debug: %ld %ld\n", q_len, t_len);
     //printf("%s\n", query_seq);
 
-    max_d = (int) (0.3*(q_len + t_len));
+    max_d = (int) (0.3 * (q_len + t_len));
 
     band_size = band_tolerance * 2;
 
@@ -137,15 +92,9 @@ alignment * align(char * query_seq, seq_coor_t q_len,
 
     k_offset = max_d;
 
-    // We should probably use hashmap to store the backtracing information to save memory allocation time
-    // This O(MN) block allocation scheme is convient for now but it is slower for very long sequences
-    d_path = calloc( max_d * (band_size + 1 ) * 2 + 1, sizeof(d_path_data2) );
-
-    aln_path = calloc( q_len + t_len + 1, sizeof(path_point) );
-
     align_rtn = calloc( 1, sizeof(alignment));
-    align_rtn->t_aln_str = calloc( q_len + t_len + 1, sizeof(char));
-    align_rtn->q_aln_str = calloc( q_len + t_len + 1, sizeof(char));
+    align_rtn->t_aln_str = NULL;
+    align_rtn->q_aln_str = NULL;
     align_rtn->aln_str_size = 0;
     align_rtn->aln_q_s = 0;
     align_rtn->aln_q_e = 0;
@@ -156,7 +105,6 @@ alignment * align(char * query_seq, seq_coor_t q_len,
     best_m = -1;
     min_k = 0;
     max_k = 0;
-    d_path_idx = 0;
     for (d = 0; d < max_d; d ++ ) {
         if (max_k - min_k > band_size) {
             break;
@@ -165,32 +113,24 @@ alignment * align(char * query_seq, seq_coor_t q_len,
         for (k = min_k; k <= max_k;  k += 2) {
 
             if ( (k == min_k) || ((k != max_k) && (V[ k - 1 + k_offset ] < V[ k + 1 + k_offset])) ) {
-                pre_k = k + 1;
                 x = V[ k + 1 + k_offset];
             } else {
-                pre_k = k - 1;
                 x = V[ k - 1 + k_offset] + 1;
             }
             y = x - k;
-            d_path[d_path_idx].d = d;
-            d_path[d_path_idx].k = k;
-            d_path[d_path_idx].x1 = x;
-            d_path[d_path_idx].y1 = y;
+            x1 = x;
+			y1 = y;
 
             while ( x < q_len && y < t_len && query_seq[x] == target_seq[y] ){
                 x++;
                 y++;
             }
 
-            d_path[d_path_idx].x2 = x;
-            d_path[d_path_idx].y2 = y;
-			if ( (x - d_path[d_path_idx].x1 > 16) && (start == false) ) {
-				align_rtn->aln_q_s = d_path[d_path_idx].x1;
-				align_rtn->aln_t_s = d_path[d_path_idx].y1;
+			if ( (x - x1 > 16) && (start == false) ) {
+				align_rtn->aln_q_s = x1;
+				align_rtn->aln_t_s = y1;
 				start = true;
 			}
-            d_path[d_path_idx].pre_k = pre_k;
-            d_path_idx ++;
 
             V[ k + k_offset ] = x;
             U[ k + k_offset ] = x + y;
@@ -228,8 +168,6 @@ alignment * align(char * query_seq, seq_coor_t q_len,
             align_rtn->aln_t_e = y;
             align_rtn->dist = d;
             align_rtn->aln_str_size = (x + y + d) / 2;
-            //d_path_sort(d_path, max_idx);
-            //print_d_path(d_path, max_idx);
             break;
         } 
     }
@@ -240,14 +178,11 @@ alignment * align(char * query_seq, seq_coor_t q_len,
 
     free(V);
     free(U);
-    free(d_path);
-    free(aln_path);
     return align_rtn;
 }
 
-
 void free_alignment(alignment * aln) {
-    free(aln->q_aln_str);
-    free(aln->t_aln_str);
+    if (aln->q_aln_str) free(aln->q_aln_str);
+    if (aln->t_aln_str) free(aln->t_aln_str);
     free(aln);
 }
