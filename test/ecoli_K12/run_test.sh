08b2870a88d90cd26a96567def5d1d8533ed96d7
Jason Chin <cschin@infoecho.net>
2019-03-16 11:53:16 -0700

    more renaming add shmr_map for quick mapping using shimmers

--- test/ecoli_K12/run_test.sh
+++ test/ecoli_K12/run_test.sh
@@ -17,17 +17,17 @@ mkdir -p $OVLOUT
 mkdir -p $ASM
 echo
 echo build read index
-time (/usr/bin/time $SHIMMERBIN/build_read_index -p $INDEX/seq_dataset -d seq_dataset.lst 2> build_db.log)
+time (/usr/bin/time $SHIMMERBIN/shmr_mkseqdb -p $INDEX/seq_dataset -d seq_dataset.lst 2> build_db.log)
 echo
 echo build shimmer index
-time (for c in `seq 1 12`; do echo "/usr/bin/time $SHIMMERBIN/build_shimmer_index -p $INDEX/seq_dataset -t 12 -c $c -o $INDEX/shimmer 2> build_index.$c.log" ; done | parallel -j 4)
+time (for c in `seq 1 12`; do echo "/usr/bin/time $SHIMMERBIN/shmr_index -p $INDEX/seq_dataset -t 12 -c $c -o $INDEX/shmr 2> build_index.$c.log" ; done | parallel -j 4)
 echo
 echo build overlaps
-time (for c in `seq -f "%02g" 1 8`; do echo "/usr/bin/time $SHIMMERBIN/shimmer_to_overlap -p $INDEX/seq_dataset -l $INDEX/shimmer-L2 -t 8 -c $c > $OVLOUT/out.$c 2> ovlp.$c.log"; done | parallel -j 4)
+time (for c in `seq -f "%02g" 1 8`; do echo "/usr/bin/time $SHIMMERBIN/shmr_overlap -p $INDEX/seq_dataset -l $INDEX/shmr-L2 -t 8 -c $c > $OVLOUT/out.$c 2> ovlp.$c.log"; done | parallel -j 4)
 echo
 echo faclon ovlp to graph
 cd $ASM
-time (cat ../ovlp/out.* | $SHIMMERBIN/dedup_overlaps > preads.ovl; echo "-" >> preads.ovl)
+time (cat ../ovlp/out.* | $SHIMMERBIN/shmr_dedup > preads.ovl; echo "-" >> preads.ovl)
 cp $SHIMMER/py/graph_to_contig.py .
 cp $SHIMMER/py/ovlp_to_graph.py .
 /usr/bin/time pypy ovlp_to_graph.py >& asm.log
