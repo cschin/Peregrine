c5f37bd61fdf7f9a7869b563e93ea93ec7133155
Jason Chin <cschin@infoecho.net>
2019-03-17 19:22:37 -0700

    minor changes for testing cases

--- test/genome_mapping/run_test.sh
+++ test/genome_mapping/run_test.sh
@@ -30,6 +30,6 @@ echo build ref shimmer index
 time (for c in `seq 1 2`; do echo "/usr/bin/time $SHIMMERBIN/shmr_index -p $INDEX/ref -t 2 -c $c -o $INDEX/ref 2> build_ref_index.$c.log" ; done | parallel -j 2)
 
 echo run shimmer_map
-$SHIMMERBIN/shmr_map -r $INDEX/ref -m $INDEX/ref-L2 -p $INDEX/seq_dataset  -l $INDEX/read-L2  -t 1 -c 1 | sort -k 4 -g -k 2 -g >  reads2ref.out
+$SHIMMERBIN/shmr_map -r $INDEX/ref -m $INDEX/ref-L2 -p $INDEX/seq_dataset  -l $INDEX/read-L2  -t 1 -c 1  >  reads2ref.out
 
 $SHIMMERBIN/shmr_map -r $INDEX/ref -m $INDEX/ref-L2 -p $INDEX/ref  -l $INDEX/ref-L2  -t 1 -c 1 > ref2ref.out
