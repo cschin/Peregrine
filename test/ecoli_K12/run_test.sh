7676b05387965b5644e5eb5ec4161f12c7a22bd3
Jason Chin <cschin@infoecho.net>
2019-04-02 21:30:27 -0700

    using installed python scripts

--- test/ecoli_K12/run_test.sh
+++ test/ecoli_K12/run_test.sh
@@ -3,7 +3,7 @@ set -e
 find ./reads/ -name "reads_*.fa" > seq_dataset.lst
 SHIMMER=../../..
 SHIMMER=$(cd "$(dirname "../../../")"; pwd)/$(basename "$1")
-SHIMMERBIN=$SHIMMER/src
+SHIMMERBIN=$SHIMMER/src/bin
 WORKDIR=$PWD/wd
 INDEX=$WORKDIR/index
 OVLOUT=$WORKDIR/ovlp
@@ -28,21 +28,14 @@ echo
 echo faclon ovlp to graph
 cd $ASM
 time (cat ../ovlp/out.* | $SHIMMERBIN/shmr_dedup > preads.ovl; echo "-" >> preads.ovl)
-cp $SHIMMER/py/graph_to_contig.py .
-cp $SHIMMER/py/ovlp_to_graph.py .
-cp $SHIMMER/py/graph_to_path.py .
-cp $SHIMMER/py/path_to_contig.py .
-cp $SHIMMER/py/cns_prototype.py .
-cp $SHIMMER/py/_falcon4py.cpython-36m-x86_64-linux-gnu.so .
-cp $SHIMMER/py/_shimmer4py.cpython-36m-x86_64-linux-gnu.so .
-/usr/bin/time pypy ovlp_to_graph.py >& asm.log
+/usr/bin/time ovlp_to_graph.py >& asm.log
 ln -sf ../index/seq_dataset.* .
 #/usr/bin/time pypy graph_to_contig.py >& to_contig.log
-/usr/bin/time pypy graph_to_path.py >& to_path.log
-/usr/bin/time python3 path_to_contig.py $INDEX/seq_dataset p_ctg_tiling_path > p_ctg.fa 2> to_contig.log
+/usr/bin/time graph_to_path.py >& to_path.log
+/usr/bin/time path_to_contig.py $INDEX/seq_dataset p_ctg_tiling_path > p_ctg.fa 2> to_contig.log
 echo $PWD/p_ctg.fa > p_ctg.lst
 time (/usr/bin/time $SHIMMERBIN/shmr_mkseqdb -p $INDEX/p_ctg -d p_ctg.lst 2> build_p_ctg_db.log)
 time (for c in `seq 1 1`; do echo "/usr/bin/time $SHIMMERBIN/shmr_index -p $INDEX/p_ctg -t 1 -c $c -o $INDEX/p_ctg 2> build_p_ctg_index.$c.log" ; done | parallel -j 4)
 time (/usr/bin/time $SHIMMERBIN/shmr_map -r $INDEX/p_ctg -m $INDEX/p_ctg-L2 -p $INDEX/seq_dataset -l $INDEX/shmr-L2 -t 1 -c 1 > read_map.txt 2> map.log)
-time (/usr/bin/time python3 cns_prototype.py $INDEX/seq_dataset $INDEX/p_ctg read_map.txt > p_ctg_cns.fa 2> cns.log)
+time (/usr/bin/time cns_prototype.py $INDEX/seq_dataset $INDEX/p_ctg read_map.txt 1 1 > p_ctg_cns.fa 2> cns.log)
 
