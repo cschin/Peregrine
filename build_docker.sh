c0abe26b8693be500722812356476e76e77449f0
Jason Chin <cschin@infoecho.net>
2019-04-07 15:52:07 -0700

    automating docker image build

---
+++ build_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pushd py
+rm -rf .eggs/ dist/ build/ peregrine.egg-info/ peregrine_pypy.egg-info get-pip.py
+popd
+
+pushd src
+make clean
+popd
+
+tar czvf src.tgz src/ falcon/ py/
+mv src.tgz docker/
+
+pushd docker/
+tag=$(git describe --abbrev=0 --tags)
+tag=${tag:2}
+docker build . --tag cschin/peregrine:${tag}
+popd
