521f8ef5faf7f3d0846b87c0081da4ee6cd70dec
Jason Chin <cschin@infoecho.net>
2019-04-18 22:23:14 -0700

    disable L1 index generation

--- test/ecoli_K12/run_test.sh
+++ test/ecoli_K12/run_test.sh
@@ -18,8 +18,8 @@ echo build read index
 time (/usr/bin/time shmr_mkseqdb -p $INDEX/seq_dataset -d seq_dataset.lst 2> build_db.log)
 echo
 echo build shimmer index
-time (for c in `seq 1 12`; do echo "/usr/bin/time shmr_index -p $INDEX/seq_dataset -t 12 -c $c -o $INDEX/shmr 2> build_index.$c.log" ; done | parallel -j 4)
-time (for c in `seq 1 12`; do echo "/usr/bin/time shmr_index -p $INDEX/seq_dataset -l 1 -t 12 -c $c -o $INDEX/shmr 2> build_index.$c.log" ; done | parallel -j 4)
+time (for c in `seq 1 12`; do echo "/usr/bin/time shmr_index -p $INDEX/seq_dataset -r 6 -t 12 -c $c -o $INDEX/shmr 2> build_index.$c.log" ; done | parallel -j 4)
+#time (for c in `seq 1 12`; do echo "/usr/bin/time shmr_index -p $INDEX/seq_dataset -l 1 -t 12 -c $c -o $INDEX/shmr 2> build_index.$c.log" ; done | parallel -j 4)
 echo
 echo build overlaps
 time (for c in `seq -f "%02g" 1 8`; do echo "/usr/bin/time shmr_overlap -p $INDEX/seq_dataset -l $INDEX/shmr-L2 -t 8 -c $c -o $OVLOUT/ovlp.$c 2> ovlp.$c.log"; done | parallel -j 4)
