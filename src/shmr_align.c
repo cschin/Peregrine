8db39757533d83857d7e489570e7ac757bc4bb6a
Jason Chin <cschin@infoecho.net>
2019-05-05 16:06:11 -0700

    put some bound on the number of max seed considered. comment out debugging output, reject links a bit ealier to save some time

--- src/shmr_align.c
+++ src/shmr_align.c
@@ -17,6 +17,8 @@
 #include <fcntl.h>
 
 
+#define MAX_SMALL_ALNS 2400
+
 shmr_aln_v * shmr_aln( 
         mm128_v *mmers0, 
         mm128_v *mmers1, 
@@ -61,6 +63,7 @@ shmr_aln_v * shmr_aln(
 
     /* loop through 2nd shimmer list to build alginements */
 	mm_idx_t ss=0;
+    uint32_t small_aln_count = 0;
 	for(;;) {
 		if (ss >= mmers1->n) break;
         if (direction == 1) {  // reversed
@@ -80,6 +83,7 @@ shmr_aln_v * shmr_aln(
             ss++;
             continue;
         }
+
         for (uint32_t i=0; i < idx_tmp -> n; i++) {
             mmer0 = mmers0->a[idx_tmp->a[i]];
             int64_t delta0, delta1;
@@ -92,13 +96,15 @@ shmr_aln_v * shmr_aln(
             uint32_t best_aln_idx = UINT32_MAX;
             double min_diff=max_diff;
             uint8_t best_found = 0;
+            small_aln_count = 0;
             for (uint32_t aln_idx = 0; aln_idx < alns->n; aln_idx ++ ){
-                printf("XX %d, %d\n", i,  aln_idx);
                 mm128_t m0, m1;
                 shmr_aln_t * aln; 
                 size_t n;
                 aln = alns->a + aln_idx;
                 n = aln->idx0.n;
+
+                if (n < 3) small_aln_count++;
 
                 if (idx_tmp->a[i] < aln->idx0.a[n-1]) continue;
 
@@ -106,13 +112,13 @@ shmr_aln_v * shmr_aln(
                 m1 = mmers1->a[aln->idx1.a[n-1]];
 
                 mm_dist = abs(mmer_pos(&mmer0) - mmer_pos(&m0));
+                if (mm_dist >= max_dist) continue;
 
                 if (direction == 1) {
                     delta1 = abs(mmer_pos(&m0) + mmer_pos(&m1));
                 } else {
                     delta1 = abs(mmer_pos(&m0) - mmer_pos(&m1));
                 }
-                mm_dist = abs(mmer_pos(&mmer0) - mmer_pos(&m0));
                 // double diff = (double) abs(delta0 - delta1) / (double) (mm_dist);
                 uint32_t diff = (uint32_t) abs( (int32_t) delta0 - (int32_t) delta1);
                 if ( diff < max_diff && diff < min_diff && mm_dist < max_dist) {
@@ -126,17 +132,18 @@ shmr_aln_v * shmr_aln(
                 aln = alns->a + best_aln_idx;
                 kv_push(mm_idx_t, 0, aln->idx0, idx_tmp->a[i]);
                 kv_push(mm_idx_t, 0, aln->idx1, s); 
-                printf("best %d %d %d\n", best_aln_idx, idx_tmp->a[i], s);
+                // printf("best %d %d %d\n", best_aln_idx, idx_tmp->a[i], s);
             } else {
                 shmr_aln_t *aln;
                 aln = calloc(sizeof(shmr_aln_t),1);
                 kv_push(mm_idx_t, 0, aln->idx0, idx_tmp->a[i]);
                 kv_push(mm_idx_t, 0, aln->idx1, s); 
                 kv_push(shmr_aln_t, 0, *alns, *aln);
-                printf("new %d %d %d\n", best_aln_idx, idx_tmp->a[i], s);
+                // printf("new %d %d %d\n", best_aln_idx, idx_tmp->a[i], s);
             }
         }
         ss++;
+        if (small_aln_count > MAX_SMALL_ALNS) break;
     }
 
 	for (khiter_t __i = kh_begin(mmidx_map); __i != kh_end(mmidx_map); ++__i) {
