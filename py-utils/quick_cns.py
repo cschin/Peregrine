7e477d590060500204906e07dc4e7e1df6c72514
Jason Chin <cschin@infoecho.net>
2019-06-10 21:38:34 -0700

    add chunking support. this does not take fasta as input, instead using a simple  format

--- py-utils/quick_cns.py
+++ py-utils/quick_cns.py
@@ -5,27 +5,31 @@ from peregrine._falcon4py import lib as falcon4py
 from peregrine._shimmer4py import ffi as shimmer_ffi
 from peregrine._shimmer4py import lib as shimmer4py
 from peregrine import utils
-from FastaReader import FastaReader
-f = FastaReader(sys.argv[1])
+
+
 read_id = None
 read_group = []
-for r in f:
-    crid = "/".join(r.name.split("/")[:2])
-    if crid != read_id:
-        if len(read_group) >= 5:
-            seqs = read_group
-            seqs.sort(key = lambda k: len(k))
-            m = int(len(seqs)/2)+1
-            seqs[0], seqs[m] = seqs[m], seqs[0]
-            cns_seq = peregrine.utils.get_cns_from_reads(seqs,
-                                                         levels=0,
-                                                         k=10,
-                                                         w=16 ,
-                                                         max_dist=150)
-            print(">"+read_id+"/qccs/")
-            print(cns_seq.decode("ascii"))
-        read_group = []
-    read_id = crid
-    if len(r.sequence) > 1000:
-        read_group.append(r.sequence.encode("ascii"))
+total_chunk = int(sys.argv[3])
+chunk = int(sys.argv[2])
+with open(sys.argv[1], "rb") as f:
+    for r in f:
+        name, seq = r.strip().split()
+        crid = b"/".join(name.split(b"/")[:2])
+        if hash(crid) % total_chunk != chunk:
+            continue
+        if crid != read_id:
+            if len(read_group) >= 3:
+                seqs = read_group
+                if len(seqs) > 20:
+                    seqs = seqs[:20]
+                cns_seq = peregrine.utils.get_cns_from_reads(seqs,
+                                                             levels=0,
+                                                             k=10,
+                                                             w=16 ,
+                                                             max_dist=150)
+                print(">"+read_id.decode("ascii")+"/qccs/ {}".format(len(seqs)))
+                print(cns_seq.decode("ascii"))
+            read_group = []
+        read_id = crid
+        read_group.append(seq)
 
